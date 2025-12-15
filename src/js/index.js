5fcafb000ef1217cbd81471857bc233db20f28b0
Paulo-Souto <phsouto16@hotmail.com>
2025-12-15 14:51:37 -0300

    primeira versão

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+botaoMostrarProjetos.addEventListen('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos, forEach(projetoInativo => {
+        projetoInativo.classlist.add('ativo');
+    });
+}
